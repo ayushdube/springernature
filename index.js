569a49b25ce0e371c3bd1a87b43d7094be74d14a
ayush dubey <ayush@ayushdubey.com>
2021-09-04 17:58:48 +0530

    updated

---
+++ index.js
@@ -0,0 +1,31 @@
+const { request } = require('express');
+const express = require('express');
+// const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+  }));
+
+app.set("view engine", "jade");
+app.use(express.json());    
+
+app.get('/', (req, res) => {
+    res.render('form')
+});
+
+app.post('/signup', (req,res) => {
+    console.log(req.body)
+
+    if(req.body.newsletter && req.body.newsletter === 'on'){
+        res.send(`Hello  ${req.body.firstname} ${req.body.lastname}, Thank you for signing up. Your account is now
+        created. You would be receiving our periodic newsletters to your email: ${req.body.email}`)
+    } else {
+        res.send(`Hello ${req.body.firstname} ${req.body.lastname}, Thank you for signing up. Your account
+        is now created`)
+    }
+});
+
+app.listen(5000, () => {
+    console.log("Server has started");
+});
